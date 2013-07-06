a056359dda69160f20d36bfdba486fdd5730dc38
Grace <tsmgrace@gmail.com>
2013-07-06 14:07:42 -0700

    index.html

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 
     var buffer = new Buffer(fs.readFileSync('index.html'));
 
-    response.send(buffer);
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
