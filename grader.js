03dba0cba8624ce82ce012e2fe2b761d93c91a24
Grace Tang <tsmgrace@gmail.com>
2013-07-15 20:09:07 -0700

    styled index

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -33,6 +34,18 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertURLExists = function(infile) {
+    var instr = infile.toString();
+    restler.get(instr).on('complete', function(result){
+        if (result instanceof Error){
+          console.log("%s encountered error. Exiting.", instr);
+          process.exit(1);
+        }
+    });
+    //console.log(instr);
     return instr;
 };
 
@@ -64,6 +77,7 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --url <html_file>', 'Path to URL', clone(assertURLExists), HTMLFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
